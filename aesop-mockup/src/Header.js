6d71ee0f402844711439a96720bf67567877039b
Student_GA113 <Student_GA113@Student-GA113.local>
2020-04-15 20:53:46 -0400

    added final component elements

--- aesop-mockup/src/Header.js
+++ aesop-mockup/src/Header.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
-import './Header.css'
+import './Header.css';
 
 class Header extends Component{
     render(){
         return (
             <div>
             <h1 class='header-text'>Complimentary standard shipping on all orders +</h1>
-            <div class="header1">
-            <div class='header-child1'>
-                <nav class='header-nav'>
-                    <ul>
-                        <li><a href="">Shop</a></li>
-                        <li><a href="">Read</a></li>
-                        <li><a href="">Stores</a></li>
-                        <li><a href="">Search</a></li>
-                    </ul>
+    <div class="header">
+
+        <div class='header-child1-nav'>
+            <nav>
+                <ul>
+                    <li>Shop</li>
+                    <li>Read</li>
+                    <li>Stores</li>
+                    <li>Search</li>
+                </ul>
             </nav>
         </div>
-     </div>
- </div>
+        <div class='header-child2-body'>
+            <div>
+                <h1> <span>Aēsop</span></h1>
+            </div>
+            <div>
+                <h2>Formulations for skin, hair, home, and body </h2>
+                <h3>Created with sensory pleasure in mind. </h3>
+            </div>
+        </div>
+        <div class='header-child3-login'>
+            <div>
+                <h1>Login</h1>
+            </div>
+        </div>
+    </div>
+    </div>
         )
     } 
 }
